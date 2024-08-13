0bb6d6326c7aa64ddbbcade0984501078f199f33
Christopher Harris <cs.harris@outlook.com>
2024-08-13 10:10:30 +0100

    storing api keys locally for security

--- fcrumours.py
+++ fcrumours.py
@@ -1,10 +1,6 @@
 import tweepy
 import tkinter
-
-consumer_key = 'U3gMFdibOGwZydmLrq4QDXND2'
-consumer_secret = 'ejUSd5ISd1q43FnCQ4ua9fMI2O5wSLDYilIa4eAAeJFtKlt2ro'
-access_token = '788354959846805504-YU1u6Of14yOr55vnEciVQfqYubDsTag'
-access_token_secret = 'ctvhZs76A0czlbObaVm5pW5O3KffGmtkgmqVHAzCdSPp0'
+from config import consumer_key, consumer_secret, access_token, access_token_secret
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
